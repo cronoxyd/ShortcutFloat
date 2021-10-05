db0b67030c9876f76a645bbe6f7f24f979882cfd
Marcel Diskowski <cronoxyd@gmail.com>
2021-10-05 18:49:04 +0200

    Various improvements

--- ShortcutFloat.Common/Services/EnvironmentMonitor.cs
+++ ShortcutFloat.Common/Services/EnvironmentMonitor.cs
@@ -14,12 +14,53 @@ namespace ShortcutFloat.Common.Services
     public class EnvironmentMonitor
     {
         private bool _running = false;
+
+        /// <summary>
+        /// Whether the <see cref="EnvironmentMonitor"/> is currently running.
+        /// </summary>
+        /// <seealso cref="Start"/>
+        /// <seealso cref="Stop"/>
         public bool Running => _running;
+
+        /// <summary>
+        /// The text in the title bar of the current foreground window.
+        /// </summary>
         public string ForegroundWindowText { get; private set; } = null;
+
+        /// <summary>
+        /// The handle of the current foreground window.
+        /// </summary>
         public IntPtr? ForegroundWindowHandle { get; private set; } = null;
+
+        /// <summary>
+        /// The bounds of the current foreground window.
+        /// </summary>
         public Rectangle? ForegroundWindowBounds { get; private set; } = null;
+
+        /// <summary>
+        /// The process of the current foreground window.
+        /// </summary>
         public Process ForegroundWindowProcess { get; private set; } = null;
-        private static Process CurrentProcess { get; } = Process.GetCurrentProcess();
+
+        /// <summary>
+        /// The process of this application (will be used e.g. for <see cref="IgnoreCurrentProcess"/>).
+        /// </summary>
+        public static Process CurrentProcess { get; } = Process.GetCurrentProcess();
+
+        /// <summary>
+        /// Specifies whether to not raise changed events when the foreground process id is 0.
+        /// </summary>
+        public bool IgnoreIdleProcess { get; set; } = true;
+
+        /// <summary>
+        /// Specifies whether to not raise changed events when the foreground process id is that of the current process.
+        /// </summary>
+        public bool IgnoreCurrentProcess { get; set; } = true;
+
+        /// <summary>
+        /// Specifies whether to not raise changed events when the handle of the foreground window is zero.
+        /// </summary>
+        public bool IgnoreZeroHandle { get; set; } = true;
 
         public event ForegroundWindowChangedEventHandler ForegroundWindowChanged = (sender, e) => { };
         public event ForegroundWindowBoundsChangedEventHandler ForegroundWindowBoundsChanged = (sender, e) => { };
@@ -34,7 +75,16 @@ namespace ShortcutFloat.Common.Services
             {
                 var currentForegroundWindowHandle = InteropServices.GetForegroundWindow();
                 _ = InteropServices.GetWindowThreadProcessId(currentForegroundWindowHandle, out uint currentForegroundWindowProcessId);
-                if (currentForegroundWindowProcessId == CurrentProcess.Id)
+
+                // Ignore current process
+                if (IgnoreCurrentProcess && currentForegroundWindowProcessId == CurrentProcess.Id)
+                    continue;
+
+                // Ignore "Idle" process
+                if (IgnoreIdleProcess && currentForegroundWindowProcessId == 0)
+                    continue;
+
+                if (IgnoreZeroHandle && currentForegroundWindowHandle == IntPtr.Zero)
                     continue;
 
                 ForegroundWindowHandle = currentForegroundWindowHandle;
@@ -42,14 +92,20 @@ namespace ShortcutFloat.Common.Services
                 _ = InteropServices.GetWindowThreadProcessId(ForegroundWindowHandle.Value, out uint procId);
                 ForegroundWindowProcess = Process.GetProcessById((int)procId);
 
-                InteropServices.GetWindowRect(ForegroundWindowHandle.Value, out RECT foregroundWindowRect);
+                _ = InteropServices.GetWindowRect(ForegroundWindowHandle.Value, out RECT foregroundWindowRect);
                 ForegroundWindowBounds = foregroundWindowRect.ToRectangle();
 
-                if (
-                    (ForegroundWindowText != lastForegroundWindowText && lastForegroundWindowText != null) ||
-                    (ForegroundWindowHandle != lastForegroundWindowHandle && lastForegroundWindowHandle != null)
-                )
+                if (ForegroundWindowText != lastForegroundWindowText && lastForegroundWindowText != null)
+                {
+                    Debug.WriteLine($"Foreground window text changed (\"{lastForegroundWindowText}\" -> \"{ForegroundWindowText}\")");
                     ForegroundWindowChanged(this, new(ForegroundWindowHandle.Value));
+                }
+
+                if (ForegroundWindowHandle != lastForegroundWindowHandle && lastForegroundWindowHandle != null)
+                {
+                    Debug.WriteLine($"Foreground window handle changed (\"{lastForegroundWindowHandle}\" -> \"{ForegroundWindowHandle}\")");
+                    ForegroundWindowChanged(this, new(ForegroundWindowHandle.Value));
+                }
 
                 if (lastWindowRects.ContainsKey(ForegroundWindowHandle.Value) && !foregroundWindowRect.Equals(lastWindowRects[ForegroundWindowHandle.Value]))
                     ForegroundWindowBoundsChanged(this, new(foregroundWindowRect.ToRectangle(), lastWindowRects[ForegroundWindowHandle.Value].ToRectangle()));
@@ -62,6 +118,9 @@ namespace ShortcutFloat.Common.Services
             }
         }
 
+        /// <summary>
+        /// Starts the monitor loop.
+        /// </summary>
         public void Start()
         {
             if (!Running)
@@ -71,6 +130,9 @@ namespace ShortcutFloat.Common.Services
             }
         }
 
+        /// <summary>
+        /// Stops the monitor loop.
+        /// </summary>
         public void Stop()
         {
             if (Running)
@@ -90,7 +152,7 @@ namespace ShortcutFloat.Common.Services
                 WindowHandle = handle;
                 WindowText = InteropServices.GetWindowTitle(handle);
 
-                InteropServices.GetWindowThreadProcessId(handle, out uint procId);
+                _ = InteropServices.GetWindowThreadProcessId(handle, out uint procId);
                 WindowProcess = Process.GetProcessById((int)procId);
             }
         }
