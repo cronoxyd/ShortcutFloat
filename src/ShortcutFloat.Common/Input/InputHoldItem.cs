49979302b7e8b6042f21c9dab3159fdde79437a7
Marcel Diskowski <cronoxyd@gmail.com>
2022-01-03 20:51:32 +0100

    Fixed TimedOut logic

--- src/ShortcutFloat.Common/Input/InputHoldItem.cs
+++ src/ShortcutFloat.Common/Input/InputHoldItem.cs
@@ -12,7 +12,14 @@ namespace ShortcutFloat.Common.Input
 
         public DateTime KeyDownTime { get; }
 
-        public bool TimedOut => (DateTime.Now - KeyDownTime).TotalSeconds > Item.HoldTimeLimitSeconds;
+        public bool TimedOut
+        {
+            get
+            {
+                if (Item.HoldTimeLimitSeconds <= 0) return false;
+                return (DateTime.Now - KeyDownTime).TotalSeconds > Item.HoldTimeLimitSeconds;
+            }
+        }
 
         public InputHoldItem(InputItem item)
         {
